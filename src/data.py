628522166bb0c973594adb5d10483884055b3c84
Aaryan Degama <aaryandegama@gmail.com>
2025-12-29 14:06:54 +0530

    I am losing my sanity

--- src/data.py
+++ src/data.py
@@ -1,26 +1,15 @@
 import os
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 6b5a171 (Implemented the init function 🦆🦆)
 Ding_dir = ".ding"
+
 
 def init():
     cwd = os.getcwd()
     ding_path = os.path.join(cwd, Ding_dir)
 
     if os.path.exists(ding_path):
-<<<<<<< HEAD
         print("It is already a ding repository")
         return
 
     os.mkdir(ding_path)
     print(f"Initialisied a ding repo in {ding_path}")
-=======
-        print("It is a ding repository")
-        return
-
-    os.mkdir(ding_path)
-    print(f"Initialisied an ding repo in {ding_path}")
->>>>>>> 6b5a171 (Implemented the init function 🦆🦆)
